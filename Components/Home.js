9b90fa4701c0199d5ea42fda2e2b6717518e0cb7
LoisCoulon <flojuve@hotmail.fr>
2021-05-31 18:05:53 +0200

    .

--- Components/Home.js
+++ Components/Home.js
@@ -1,29 +1,59 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { Component } from 'react';
-import { Alert, Modal, StyleSheet, Text, View, ListView} from 'react-native';
+import React, { useState } from 'react';
+import { Alert, Modal, StyleSheet, Text, View,Pressable, ListView} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { withSafeAreaInsets } from 'react-native-safe-area-context';
+import { MaterialIcons } from '@expo/vector-icons';
 import "./Home.scss";
 
 
 
 export default function Home({ navigation }) {
 
-  const promo = ["promo1", "promo2", "promo3", "promo4", "promo5"];
+//   const promotion = useState([
+//     {id:1, title:"promo1", content: "-50% sur les chaussettes" },
+//     {id:2, title:"promo2", content: "-50% sur les chemises" },
+//     {id:3, title:"promo3", content: "-50% sur les jeans" }
+// ]);
+  const [modalOpen, setModalOpen] = useState(false);
+  const promo = ["promo1"]
+  const promoOld = ["promo2"]
+  const ref = ["-50% sur les t-shirt"]
+
   const listItems = promo.map((promo) =>
   <li>{promo}</li>
-);
+  );
+  const listItems2 = promoOld.map((promoOld) =>
+    <li>{promoOld}</li>
+  );
 
     return (
-      <View style={ styles.container }>
-          <Text style={styles.h1}>Bienvenue sur Go Style</Text>
-          <View style={styles.tab}>
-            <Text style={styles.promo}>Tableau de vos promotions :</Text>
-              <ul>{listItems}</ul>
-            <Text style={styles.cell}>Scanner un QRCode</Text>
-          </View>
+    <View style={ styles.container }>
+      <Text style={styles.h1}>Bienvenue sur Go Style</Text>
+      <View style={styles.tab}>
+        <Text style={styles.promo}>Tableau de vos promotions :</Text>
+        <Text style={styles.title}>En cours</Text>
+   
+          <Modal visible={modalOpen} animationType="slide">
+            <View style={styles.modalContent}>
+              <MaterialIcons
+                name="close"
+                size={24}
+                style={styles.modalToggle}
+                onPress={() => setModalOpen(false)}          
+              />
+              <Text>{ref}</Text>
+            </View>
+          </Modal>
+
+        <Text style={styles.modalToggle} onPress={() => setModalOpen(true)}>{listItems}</Text>
+
+        <Text style={styles.title}>Passées</Text>
+        <Text style={styles.modalToggle} onPress={() => setModalOpen(true)}>{listItems2}</Text>
+        <Text style={styles.cell}>Scanner un QRCode</Text>
       </View>
+    </View>
     );
 }
 
@@ -59,7 +89,27 @@ export default function Home({ navigation }) {
       fontSize: "35px",
       position: "absolute",
       top: 0
+    },
+    modalToggle:{
+      marginBottom: 10,
+      borderWidth:1,
+      borderColor:"#f2f2f2",
+      padding:10,
+      alignSelf:'start',
+      listStyle:"none",
+      marginTop:'10px',
+      marginLeft:"15px"
+    },
+    modalContent:{
+      flex:1,
+      textAlign:"center",
+      marginTop:20,
+    },
+    title:{
+      fontStyle:'italic',
+      marginTop:'10px',
+      marginLeft:"15px"
     }
     
-  });
+});
   
